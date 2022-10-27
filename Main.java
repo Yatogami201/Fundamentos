ffcf8d78244f24a3eb71bfba5fdc0c6efbeb2ffb
Yatogami201 <110358325+Yatogami201@users.noreply.github.com>
2022-10-27 09:47:21 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,12 @@
 import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        char[] text = userInput.toCharArray();
-        int counter = 0;
-        
-        for (char letter : text) {
-            if (letter == 'a') {
-                counter++;
-            }
-        }
-        
-        System.out.println(counter);
-    }
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String entrada = scan.nextLine();
+    String a = entrada.replaceAll("Quiere Cacao","");
+    System.out.println(a);
+    
+      
+    
+  }
 }
